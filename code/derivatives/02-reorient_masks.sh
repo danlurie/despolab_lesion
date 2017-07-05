98ea3f56c4edd912b3c9586ac7a77136133a8f5b
danlurie <danjlurie@gmail.com>
2017-07-05 14:49:23 -0700

    Removing old pointers to files that have been carelessly renamed or moved.

--- code/derivatives/02-reorient_masks.sh
+++
@@ -1,8 +0,0 @@
-for pid in `cat /home/despoB/dlurie/Projects/despolab_lesion/lesion_masks/has_bids_mask.txt`; do
-    echo "Reorienting lesion mask for subject  $pid...";
-    # Reorient lesion mask and write to derivatives directory.
-    python ~/Software/Scripts/nb_reorient.py \
-        /home/despoB/lesion/data/original/bids/sub-${pid}/anat/sub-${pid}_t1w_label-lesion_roi.nii.gz \
-        /home/despoB/dlurie/Projects/despolab_lesion/derivatives/sub-${pid}/anat/sub-${pid}_t1w_variant-reorient_label-lesion_roi.nii.gz;
-done
-
--- view_patient.sh
+++
@@ -1,2 +0,0 @@
-sub_id=$1
-fslview /home/despoB/lesion/anat_preproc/$sub_id/${sub_id}_quick_reg_InverseWarped.nii.gz /home/despoB/lesion/anat_preproc/$sub_id/${sub_id}_mask_mni.nii.gz
